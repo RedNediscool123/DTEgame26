c876e439033ab675b109f2cc5f6dcf6560f86f3e
Red_Nediscool123 <nedsta36@kingshigh.school.nz>
2026-05-26 09:54:20 +1200

    big code block for movement and start of attack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,43 +1,45 @@
 extends CharacterBody2D
 
-
-
 const SPEED = 100.0
-
 var last_direction: Vector2 = Vector2.RIGHT
-
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	
+	if Input.is_action_just_pressed("attack"):
+		attack()
+	
+	
 	move_and_slide()
 	process_movement()
 
-
-
 func process_movement() -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("left" , "right" , "up" , "down")
-	
+	var direction := Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
-	
 	process_animation(direction)
 
-func process_animation(direction) -> void:
-	if velocity != Vector2.ZERO:
+func process_animation(direction: Vector2) -> void:
+	if direction != Vector2.ZERO:
+		last_direction = direction  # Update last direction while moving
 		play_animation("run", direction)
-	else: 
-		play_animation("idle", direction)
+	else:
+		play_animation("idle", last_direction)  # Use last_direction when idle
 
 func play_animation(prefix: String, dir: Vector2) -> void:
 	if dir.x > 0:
 		animated_sprite_2d.play(prefix + "_right")
+	elif dir.x < 0:
+		animated_sprite_2d.play(prefix + "_left")
 	elif dir.y < 0:
 		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down")
-	elif dir.x < 0:
-		animated_sprite_2d.play(prefix + "_left")
 		
+		
+		
+#attacking
+
+func attack() -> void:
+	play_animation("attack", last_direction)
+	print("ATTACK!!!")
