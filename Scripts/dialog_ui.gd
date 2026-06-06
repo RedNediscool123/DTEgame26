a7a16e085d96a07750c8667cc89460f6d25c0b23
RedNediscool123 <nedsta36@kingshigh.school.nz>
2026-06-06 22:54:36 +1200

    finished character talking sprite changes

--- Scripts/dialog_ui.gd
+++ Scripts/dialog_ui.gd
@@ -3,9 +3,32 @@ extends Control
 @onready var dialogue_text = %dialogue_text
 @onready var speaker_name = %speaker_name
 
-
+const ANIMATION_SPEED : int = 30
+var animate_text : bool = false
+var current_visible_characters : int = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+
+func _process(delta):
+	if animate_text:
+		if dialogue_text.visible_ratio < 1:
+			dialogue_text.visible_ratio += (1.0/dialogue_text.text.length()) * (ANIMATION_SPEED * delta)
+			current_visible_characters = dialogue_text.visible_characters
+		else:
+			animate_text = false
+
+
+
+func change_line(speaker : String, line : String):
+	speaker_name.text = speaker
+	current_visible_characters = 0
+	dialogue_text.text = line
+	dialogue_text.visible_characters = 0
+	animate_text = true
+	
+func skip_text_animation():
+	dialogue_text.visible_ratio = 1
--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -7,8 +7,24 @@ var dialog_index : int = 0
 
 
 const dialogue_text : Array[String] = [
+	"Lizard: Hello there! I'm here! over here, I'm just a bit small", 
 	"Lizard: Hello, nice to finally meet you, press enter or space, or just click to continue", 
-	"Lizard: Wow! you're getting the hang of this already!"
+	"Lizard: Wow! you're getting the hang of this already!",
+	"Lizard: You're probably wondering why you're here, aren't you?",
+	"Lizard: Well I can tell you! In fact i'd be happy to tell you",
+	"Jelly: HOLD IT!",
+	"Lizard: JELLY?!", 
+	"Jelly: You slimy lizard! I'll get you back this time!",
+	"Jelly: ...", 
+	"Jelly: Wait... Who's that?",
+	"Jelly: Ugh! Nevermind! I'll deal with you stupid lizard first!",
+	"Jelly: Huh! He's gone already! This can't be!",
+	"Jelly: Catch you later, stranger...", 
+	"Jelly: GET BACK HERE YOU QUADRUPEDAL PEST!!", 
+	"You: Well that was strange...",
+	"Rocky: Hello stranger! Wait.. why are you in our house?",
+	"Rocky: Oh well, have you seen a lizard and a jelly person around? I'm kinda looking for them",
+	
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -19,9 +35,13 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("next_line"):
-		if dialog_index < len(dialogue_text) - 1:
-			dialog_index += 1
-			process_current_line()
+		if dialogui.animate_text:
+			dialogui.skip_text_animation()
+		else:
+			if dialog_index < len(dialogue_text) - 1:
+				dialog_index += 1
+				process_current_line()
+
 
 
 
@@ -37,6 +57,6 @@ func parse_line(line: String):
 func process_current_line():
 	var line = dialogue_text[dialog_index]
 	var line_info = parse_line(line)
-	dialogui.speaker_name.text = line_info["speaker_name"]
-	dialogui.dialogue_text.text = line_info["dialogue_text"]
+	dialogui.change_line(line_info["speaker_name"], line_info["dialogue_text"])
+	character.change_character(line_info["speaker_name"])
 	
