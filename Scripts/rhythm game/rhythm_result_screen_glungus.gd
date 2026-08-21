9a15d30fa3ca67ef47e83785aeea2bc4db5fa8aa
RedNediscool123 <nedsta36@kingshigh.school.nz>
2026-08-21 14:50:35 +1200

    levels

---
+++ Scripts/rhythm game/rhythm_result_screen_glungus.gd
@@ -0,0 +1,24 @@
+extends Control
+func _ready():
+	%AudioStreamPlayer2D.play()
+	$ScoreLabel.text = "Score: " + str(RhythmGameSignals.final_score) + " pts"
+	$BestComboLabel.text = "Best Combo: " + str(RhythmGameSignals.best_combo) + "x"
+	
+	$PlayAgainButton.pressed.connect(_on_play_again_pressed)
+	$QuitButton.pressed.connect(_on_quit_pressed)
+	%DoneButton.pressed.connect(_on_done_pressed)
+	
+	%DoneButton.visible = RhythmGameSignals.final_score > 52000
+
+func _on_play_again_pressed():
+	RhythmGameSignals.final_score = 0
+	RhythmGameSignals.best_combo = 0
+	get_tree().change_scene_to_file("res://Scenes/rhythm game/rhythm_game_level_grief.tscn")
+func _on_quit_pressed():
+	RhythmGameSignals.final_score = 0
+	RhythmGameSignals.best_combo = 0
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+func _on_done_pressed():
+	RhythmGameSignals.final_score = 0
+	RhythmGameSignals.best_combo = 0
+	get_tree().change_scene_to_file("res://Scenes/text game/grief_route.tscn")
