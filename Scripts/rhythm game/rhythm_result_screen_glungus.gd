2bcf6c394d059b622f4f043435c4fd6e4a17898a
RedNediscool123 <nedsta36@kingshigh.school.nz>
2026-08-25 09:56:22 +1200

    desert update

--- Scripts/rhythm game/rhythm_result_screen_glungus.gd
+++ Scripts/rhythm game/rhythm_result_screen_glungus.gd
@@ -13,7 +13,7 @@ func _ready():
 func _on_play_again_pressed():
 	RhythmGameSignals.final_score = 0
 	RhythmGameSignals.best_combo = 0
-	get_tree().change_scene_to_file("res://Scenes/rhythm game/rhythm_game_level_grief.tscn")
+	get_tree().change_scene_to_file("res://Scenes/rhythm game/rhythm_game_level_glungus.tscn")
 func _on_quit_pressed():
 	RhythmGameSignals.final_score = 0
 	RhythmGameSignals.best_combo = 0
